834b3f4c453ffa0ff3766faa176511a268ec4c12
afonsoabc36 <silva.af36@gmail.com>
2024-02-08 00:25:31 +0000

    Visual changes to the html files

--- Aula1/script.py
+++ Aula1/script.py
@@ -1,3 +1,4 @@
+import shutil
 import json
 import os
 
@@ -9,17 +10,18 @@ html = """
     <meta charset="UTF-8"/>
 </head>
 <body>
-
 """
 
 template = html
 
 file = open("mapa.json", "r", encoding="utf-8").read()
+if os.path.exists("html"):
+    shutil.rmtree("html")
 os.mkdir("html")
 
 content = json.loads(file)
 
-html += "<ul>\n"
+html += "\t<ul>\n"
 
 listaCidades = []
 
@@ -27,13 +29,13 @@ for elem in content["cidades"]:
     listaCidades.append(elem["nome"])
 
     templateCidade = template
-    templateCidade += f"<h1>{elem['nome']}</h1>\n"
-    templateCidade += f"<h3>{elem['distrito']}</h3>\n"
-    templateCidade += f"<p><b>População: </b>{elem['população']}</p>\n"
-    templateCidade += f"<p><b>Descrição: </b>{elem['descrição']}</p>\n"
-    templateCidade += f"<h5> <a href='../mapa_sorted.html'>Voltar</a> <h5>\n"
+    templateCidade += f"\t<h1>{elem['nome']}</h1>\n"
+    templateCidade += f"\t<h3>{elem['distrito']}</h3>\n"
+    templateCidade += f"\t<p><b>População: </b>{elem['população']}</p>\n"
+    templateCidade += f"\t<p><b>Descrição: </b>{elem['descrição']}</p>\n"
+    templateCidade += f"\t<h5><a href='../mapa_sorted.html'>Voltar</a><h5>\n"
     templateCidade += "</body>\n"
-    templateCidade += f"</html>\n"
+    templateCidade += "</html>\n"
 
     fileCidade = open(f"html/{elem['nome']}.html", "w", encoding="utf-8")
     fileCidade.write(templateCidade)
@@ -42,9 +44,9 @@ for elem in content["cidades"]:
 
 
 for elem in sorted(listaCidades):
-    html += f"<li><a href='html/{elem}.html'>{elem}</a></li>\n"
+    html += f"\t\t<li><a href='html/{elem}.html'>{elem}</a></li>\n"
 
-html += "</ul>\n"
+html += "\t</ul>\n"
 
 html += "</body>\n"
 html += "</html>"
